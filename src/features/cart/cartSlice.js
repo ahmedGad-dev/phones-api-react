bd38e8bb888fe30a674547fb49107798958fe10e
ahmedGad-dev <76513984+ahmedGad-dev@users.noreply.github.com>
2023-01-20 18:26:49 +0200

    initialized app

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { openModal } from '../modal/modalSlice';
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
+const initialState = {
+  cartItems: [],
+  amount: 4,
+  total: 0,
+  isLoading: true,
+};
+
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (name, thunkAPI) => {
+    try {
+      // console.log(name);
+      // console.log(thunkAPI);
+      // console.log(thunkAPI.getState());
+      // thunkAPI.dispatch(openModal());
+      const resp = await axios(url);
+
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {}
+});
+
+// console.log(cartSlice);
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
